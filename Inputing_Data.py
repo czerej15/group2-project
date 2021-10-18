d57327ca0508baa60fcefd749ec52cf7b70b6a7e
Keely Smith <keely.smith1@icloud.com>
2021-10-18 14:58:29 +1100

    changing file locations

--- Inputing_Data.py
+++ Inputing_Data.py
@@ -1,1 +1,11 @@
-print('hello world')
+print('Opening file myfile.txt.')
+f = open('student_data.csv')  # create file object
+
+print('Reading file myfile.txt.')
+contents = f.read()  # read file text into a string
+
+print('Closing file myfile.txt.')
+f.close()  # close the file
+
+print('\nContents of myfile.txt:')
+print(contents)
