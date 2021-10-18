341b3970514cf5a41911096d34eaefa9a363207a
James Czeredys <czerej15@outlook.com>
2021-10-18 17:58:01 +1100

    added to menu

--- Main.py
+++ Main.py
@@ -27,38 +27,75 @@ if __name__ == "__main__":
 		print("1 - Student Login")
 		print("2 - System Login ")
 		print("3 - Exit ")
-		option = input("Press the respective key to navigate:")
+		main_opt = input("Press the respective key to navigate:")
 
-		if option == '3': # exit
+		if not checkValidOptionNumb(main_opt, 3):
+			continue
+
+		if main_opt == '3': # exit
 			print('Goodbye.')
 			break 
 
-		if option == '1': # student
-			print("----------------------------------------")
-			id = input("Enter student ID:")
-			if checkStudentID(id): # returns true if valid
-				student =  findStudentByID(id, student_list) #student_list is a list that just contains the Student instances. Probably created in "Inputing_Data.py"
-				if student == False:
-					print("Invalid ID")
+		if main_opt == '1': # student
+			while True:
+				print("----------------------------------------")
+				print("1 - Enter student ID")
+				print("2 - Exit")
+				sID_opt = input("Press the respective key to navigate:")
+				if not checkValidOptionNumb(sID_opt, 2):
 					continue
-				else:
-					print("----------------------------------------")
-					# print student info
-					print("Name:", student.name)
-					print("Student ID:", student.studentID)
-					print("Date of Birth:", student.dob)
-					#print(student.programCode)
-					# student menu options
-					print("1 - Display academic history and current enrolment")
-					print("2 - Querying course or program information")
-					print("3 - Enrol/UnEnrol in a current offering")
-					print("4 - Exit")
-					option = input("Press the respective key to navigate:")
+				
+				if sID_opt == '2': #exit
+					break
+				
+				if sID_opt == "1":
+					id = input("Enter student ID:")
+					if checkStudentID(id): # returns true if valid
+						#student_list is a list that just contains the Student instances. Probably created in "Inputing_Data.py"
+						student = findStudentByID(id, student_list)
+						
+						if student == False: # if student not returned
+							print("Student not found.")
+							continue
+						
+						print("----------------------------------------")
+						# print student info
+						print("Name:", student.name)
+						print("Student ID:", student.studentID)
+						print("Date of Birth:", student.dob)
+						print("Student Menu:")
+						#print(student.programCode)
+						# student menu options
+						print("1 - Display academic history and current enrolment")
+						print("2 - Querying course or program information")
+						print("3 - Enrol/UnEnrol in a current offering")
+						print("4 - Exit")
+						stud_opt = input("Press the respective key to navigate:")
+						if not checkValidOptionNumb(stud_opt, 4):
+							continue
 
-				if option == 4:
+						if stud_opt == 4: #exit
+							break
+
+
+		if main_opt == '2':
+			while True:
+				# system login
+				print("----------------------------------------")
+				print("Admin Menu:")
+				print("1 - Add/Remove/Amend a student")
+				print("2 - Add/Remove/Amend a course")
+				print("3 - Add/Remove/Amend a program")
+				print("4 - Add/Remove/Amend a semester")
+				print("5 - Query student information")
+				print("6 - Allow manual amendment of the study plan for a student")
+				print("7 - Validating a student’s study plan")
+				print("8 - Generate a study plan for a student")
+				print("9 - Display list of student achievements for course")
+				print("10 - Exit")
+				admin_opt = input("Press the respective key to navigate:")
+				if not checkValidOptionNumb(admin_opt, 10):
+					continue
+				if admin_opt == '10':
 					break
 
-
-		if option == '2':
-			# system login
-			pass
